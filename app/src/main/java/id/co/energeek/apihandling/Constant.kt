4d40b683e4f29fef1c9e7f693ed24d32533b6346
Melia Fitriawati <meliafitriawati@gmail.com>
2019-03-23 11:04:46 +0700

    base_url

--- app/src/main/java/id/co/energeek/apihandling/Constant.kt
+++ app/src/main/java/id/co/energeek/apihandling/Constant.kt
@@ -1,4 +1,3 @@
 package id.co.energeek.apihandling
 
-const val BASE_URL_DEBUG: String = "https://api.myjson.com/bins/"
-//const val BASE_URL_RELEASE: String = "https://api.myjson.com/bins/release"
+const val BASE_URL: String = "https://api.myjson.com/bins/"
